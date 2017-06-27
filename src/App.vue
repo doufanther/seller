de5bb72b20af72f7b9caf143c6fedf2c436b65c5
doufanther <zhengshufa@126.com>
2017-06-27 17:57:05 +0800

    6-27   17：56

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-  	<v-header></v-header>
+  	<v-header :seller="seller"></v-header>
   	<div class="tab">
   		<div class="tab-item"><router-link to="/goods">商品</router-link></div>
   		<div class="tab-item"><router-link to="/ratings">评论</router-link></div>
@@ -12,16 +12,48 @@
 
 <script>
     import header from './components/header/header.vue'
+    const ERR_OK=0
 		export default{
-		    components:{
-			      'v-header':header
-			  }
+			data() {
+				return{
+					seller:{}
+				}
+			},
+			created(){
+				this.$http.get('/api/seller').then((response)=>{
+					response=response.body;
+					if(response.errno===ERR_OK){
+						this.seller=response.data;
+						//console.log(this.seller);
+					}
+				})
+			},
+		  components:{
+			  	'v-header':header
+			}
 	  }
 </script>
 
-<style>
+<!--<style>
 	#app .tab{display: flex;width:100%;height:40px;line-height:40px;border-bottom: 1px solid rgba(7,17,27,.1);}
 	#app .tab .tab-item{flex: 1;text-align: center;}
 	#app .tab .tab-item a{display:block;font-size: 14px;color: rag(77,85,93);}
 	#app .tab .tab-item a.active{color:rgb(240,20,20);}
+</style>-->
+<style lang="scss" scoped type="text/css">
+	$fontSize:14px;
+	#app{
+	  .tab{
+	    display: flex;width:100%;height:40px;line-height:40px;border-bottom: 1px solid rgba(7,17,27,.1);
+	    .tab-item{
+	    	flex: 1;text-align: center;
+	    	a{
+	    		display:block;font-size: $fontSize;color: rag(77,85,93);
+	    	}
+	    	a.active{color:rgb(240,20,20);}
+	    }
+	  }
+	}
+	  
+	    
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,17 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import VueResource from 'vue-resource';
 import goods from'../components/goods/goods.vue'
 import ratings from'../components/ratings/ratings.vue'
 import seller from'../components/seller/seller.vue'
 Vue.use(Router);
-
+Vue.use(VueResource);
 
 export default new Router({
+	mode:'history',
+	scrollBehavior:()=>({
+		y:0
+	}),
 	linkActiveClass:'active',
   routes: [
     {
@@ -20,6 +25,10 @@ export default new Router({
     {
     	path:'/seller',
     	component:seller
+    },
+    {
+    	path:'*',
+    	redirect:'/goods'
     }
   ]
 })
